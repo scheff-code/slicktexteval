91357f3e333222cb213953ddfdc0eaa23d485829
dscheff <55660347+scheff-code@users.noreply.github.com>
2024-12-01 18:10:28 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ .'/../utilities.php';
-// require_once __DIR__ .'/../config.php';
 require_once __DIR__ .'/../App/Models/DB.php';
 require_once __DIR__ .'/../App/Validation.php';
 require_once __DIR__ .'/../App/Models/User.php';
